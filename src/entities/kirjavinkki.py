9d6cd67dcbf3a42033076ed32c7e8de623dc0bf4
TuomoM <tuomo.mehtala@gmail.com>
2022-03-31 22:26:17 +0300

    kirjavinkki luokka luotu

---
+++ src/entities/kirjavinkki.py
@@ -0,0 +1,10 @@
+#
+class kirjavinkki:
+    def __init__(self, otsikko,kirjailia = "tuntematon",kirjoitusvuosi = "1981", omistaja = "käyttäjä"):
+        self.otsikko = otsikko
+        self.kirjailia = kirjailia
+        self.kirjoitusvuosi = kirjoitusvuosi
+        self.omistaja = omistaja
+        self.tyyppi = "kirja"
+        
+    
