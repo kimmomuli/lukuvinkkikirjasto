247e5f884f079947c9ca09d1c341ea0015aeae74
TuomoM <tuomo.mehtala@gmail.com>
2022-04-06 16:50:43 +0300

    Kayttäjärepo luotu, testaus puuttuu vielä

---
+++ src/repositories/kayttajat.py
@@ -0,0 +1,32 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+import database
+
+
+def luo_kayttaja(kayttajatunnus: str, salasana: str):
+    sql = "SELECT tunnus FROM kayttajat where tunnus = tunnus:"
+    kayttajatietue = database.session.execute(sql, {"tunnus": kayttajatunnus})
+    if kayttajatietue.fetchOne():
+        raise Exception("Käyttäjätunnus on jo olemassa")
+    if len(salasana) < 6:
+        raise Exception("Salasanan on oltava vähintään 6 merkkiä pitkä")
+    if len(kayttajatunnus) < 6:
+        raise Exception(
+            "Kayttäjätunnukset on oltava vähintään 6 merkkiä pitkä")
+    salasana_hash = generate_password_hash(salasana)
+    sql2 = "INSERT into kayttajat (tunnus, password) VALUES (:tunnus, :password)"
+    database.session.execute(
+        sql2, {"tunnus": kayttajatunnus, "password": salasana_hash})
+    database.session.commit()
+
+
+def login(kayttajatunnus: str, salasana: str) -> bool:
+    sql = "SELECT tunnus,password FROM kayttajat where tunnus = tunnus:"
+    kayttajatietue = database.session.execute(
+        sql, {"tunnus": kayttajatunnus}).fetchOne()
+    if kayttajatietue:
+        if(check_password_hash(kayttajatietue[1], salasana)):
+            return True
+        else:
+            return False
+    else:
+        raise Exception("käyttäjätunnusta ei ole rekisteröity")
