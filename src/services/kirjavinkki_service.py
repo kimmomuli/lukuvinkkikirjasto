284987115c9ee60f685638e78bf7bd3b543f10a1
Kimmo Muli <kimmo.muli@hotmail.com>
2022-04-11 13:50:15 +0300

    korjaus kirjavinkin tarkistukseen

--- src/services/kirjavinkki_service.py
+++ src/services/kirjavinkki_service.py
@@ -15,13 +15,16 @@ def tarkista_numeromuoto(numero) -> bool:
 
 
 def tarkista_kirjavinkki(otsikko, kirjailija, kirjoitusvuosi):
-    # Otsikko ja kirjailija voivat sisältää numeroita, kun ne vain ovat merkkijonomuodossa!
 
-    if not tarkista_merkkijonomuoto(otsikko) and len(otsikko) < 2:
-        return "Otsikon pitää olla merkkijono ja sen tulee sisältää ainakin kaksi merkkiä"
+    if not tarkista_merkkijonomuoto(otsikko):
+        return "Otsikon pitää olla merkkijono"
+    elif len(otsikko) < 2:
+        return "Otsikon tulee sisältää ainakin kaksi merkkiä"
 
-    if not tarkista_merkkijonomuoto(kirjailija) and len(kirjailija) < 2:
-        return "Kirjailijan pitää olla merkkijono ja sen tulee sisältää ainakin kaksi merkkiä"
+    if not tarkista_merkkijonomuoto(kirjailija):
+        return "Kirjailijan pitää olla merkkijono"
+    elif len(kirjailija) < 2:
+        return "Kirjailijan tulee sisältää ainakin kaksi merkkiä"
 
     if not tarkista_numeromuoto(kirjoitusvuosi):
         return "Kirjoitusvuosi pitää olla numero"
