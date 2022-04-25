4ecc8f0ae4508651b3b9d62df9c098d5d033ab9f
Juho Röyskö <roysko.juho@gmail.com>
2022-04-25 16:50:49 +0300

    Add desired functionality to sorting buttons

--- src/views/tips.py
+++ src/views/tips.py
@@ -6,11 +6,14 @@ tips_bp = Blueprint("tips", __name__)
 
 @tips_bp.route("/", methods=["GET", "POST"])
 def tips_route():
-    tips = tip_repository.get_all_tips()
+    order = request.args.get("order") if request.args.get("order") else "level"
+    tips = tip_repository.get_all_tips(order)
+
     if request.method == "POST":
         for tip in tips:
             if (request.form.get(f"delete_{tip.type}_{tip.title}")
                     and tip.adder_username == session["username"]):
                 tip_repository.delete_tip(tip)
-        tips = tip_repository.get_all_tips()
+        tips = tip_repository.get_all_tips(order)
+
     return render_template("tips.html", tips=tips)
