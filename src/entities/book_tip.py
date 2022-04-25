56c215ca5ea9e9de6c048d21347ea3ab46f84433
Juho Röyskö <roysko.juho@gmail.com>
2022-04-25 14:31:52 +0300

    Make BookTip eq and hash functions also compare likes

--- src/entities/book_tip.py
+++ src/entities/book_tip.py
@@ -10,14 +10,18 @@ class BookTip:
         self.adder_username = adder_username
         self.type = "book"
         self.timestamp = timestamp
-        self.likes = likes
+        self.likes = likes if likes else []
 
     def __eq__(self, other: "BookTip") -> bool:
         return (self.title == other.title
                 and self.author == other.author
                 and self.year == other.year
                 and self.adder_username == other.adder_username
-                and self.type == other.type)
+                and self.type == other.type
+                and self.likes == other.likes)
 
     def __hash__(self):
-        return hash((self.title, self.author, self.year, self.adder_username, self.type))
+        likes_hash = 0
+        for like in self.likes:
+            likes_hash += hash(like)
+        return hash((self.title, self.author, self.year, self.adder_username, self.type, likes_hash))
